ca7cf91ef04b9529909b70a69ddfdd5cca854a6f
Jae-yi <plotojjy@gmail.com>
2025-02-20 10:32:19 +0900

    CORS config commit

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -1,12 +1,13 @@
 package com.bookmarker.api.controller;
 
+import com.bookmarker.api.dto.BookmarkDTO;
 import com.bookmarker.api.dto.BookmarksDTO;
+import com.bookmarker.api.dto.CreateBookmarkRequest;
 import com.bookmarker.api.service.BookmarkService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -15,13 +16,32 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
+    /**
+     * Bookmark 목록 및 검색
+     * @param page
+     * @param query
+     * @return
+     */
     @GetMapping
     public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page,
                                      @RequestParam(name = "query", defaultValue = "") String query) {
-        // query param 이 비어있으면 그냥 리스트만
+
+        // query param (검색어) 비어있으면 그냥 리스트만 호출
         if(query == null || query.isBlank()) {
             return bookmarkService.getBookmarks(page);
         }
         return bookmarkService.searchBookmarks(query, page);
+    }
+
+    /**
+     * Bookmark 등록
+     * @param request
+     * @return
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    // @Valid : CreateBookmarkRequest에 선언한 어노테이션에 따라 잘 들어왔는지 DataBinding을 체크해주는 Validator 호출
+    public BookmarkDTO createBookmark(@RequestBody @Valid CreateBookmarkRequest request) {
+        return bookmarkService.createBookmark(request);
     }
 }
--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -1,10 +1,8 @@
 package com.bookmarker.api.service;
 
+import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
-import com.bookmarker.api.dto.BookmarkDTO;
-import com.bookmarker.api.dto.BookmarkMapper;
-import com.bookmarker.api.dto.BookmarkVM;
-import com.bookmarker.api.dto.BookmarksDTO;
+import com.bookmarker.api.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 @Transactional
@@ -21,8 +21,14 @@ public class BookmarkService {
     private final BookmarkRepository repository;
     private final BookmarkMapper mapper;
 
+    /**
+     * Bookmark 목록 조회
+     * @param page
+     * @return
+     */
     @Transactional(readOnly = true)
     public BookmarksDTO<?> getBookmarks(Integer page) {
+        // JPA Paging은 0부터 시작!
         int pageNo = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
 
@@ -39,6 +45,12 @@ public class BookmarkService {
         return new BookmarksDTO(bookmarkPage);
     }
 
+    /**
+     * Bookmark 검색
+     * @param query
+     * @param page
+     * @return
+     */
     @Transactional(readOnly = true)
     public BookmarksDTO<?> searchBookmarks(String query, Integer page) {
         int pageNo = page < 1 ? 0 : page - 1;
@@ -46,12 +58,23 @@ public class BookmarkService {
         //1. 기존 메소드
         // Page<BookmarkDTO> bookmarkPage = repository.searchBookmarks(query, pageable);
 
-        //2.repository 기존 메소드 대체하는 JPA Query method
+        //2. 1번 메소드 대체하는 JPA Query method
         /* Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
         return new BookmarksDTO(bookmarkPage);*/
 
-        //3. BookmarkVM I/F Injection
+        //3. 1번 메소드 대체하는 BookmarkVM I/F Injection
         Page<BookmarkVM> bookmarkPage = repository.findByTitleContainingIgnoreCase(query, pageable);
         return new BookmarksDTO<>(bookmarkPage);
+    }
+
+    /**
+     * Bookmark 등록
+     * @param request
+     * @return
+     */
+    public BookmarkDTO createBookmark(CreateBookmarkRequest request) {
+        Bookmark bookmark = new Bookmark(request.getTitle(), request.getUrl(), Instant.now());
+        Bookmark savedBookmark = repository.save(bookmark);
+        return mapper.toDTO(savedBookmark);
     }
 }
